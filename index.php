fa29751d6109ddb23d28e79e62b65b32a58a3763
Christine Nichols <cpichols@gmail.com>
2020-03-07 15:59:04 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/* this is a starter page */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body id="home">
+    <div class="header">
+        <h1>Indiana Powerlifting</h1>
+        <h3>Where NASA Sports makes it's home in Indiana</h3>
+    </div>
+    <div class="splash">
+        <h4>This is where we'll have a slider with Indiana Lifting on display</h4>
+    </div>
+    <div class="spotlight">
+        <h4>Spotlight - Happenings, Links, Lifters, and Whatnot</h4>
+    </div>
+    <div class="records">
+        <h2>NASA Records and Awards</h2>
+        <ul>
+            <li>Indiana Lifters</li>
+            <li>Ohio Lifters</li>
+            <li>Illinois Lifters</li>
+            <li>Wisconsin Lifters</li>
+            <li>Kentucky Lifters</li>
+            <li>National Records and Awards</li>
+        </ul>
+    </div>
+    <div class="howto">
+        <h2>NASA - the Natural Athlete Strength Association</h2>
+        <h3>Introduction</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ea error dolorum! Consequuntur ipsa velit dicta ut. Possimus voluptas facilis corporis totam ipsam magnam corrupti non, dolore nobis dicta odit labore, aspernatur esse ad ducimus laudantium. Nam laudantium necessitatibus porro.</p>
+        <h3>Want to Compete in Powerlifting? Here's how to get started!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur officia id voluptate mollitia voluptatum, voluptatibus magnam, numquam quia asperiores similique eveniet? Tempore omnis provident totam officia nihil architecto ex eveniet.</p>
+        <h3>Lifting Blog Posts</h3>
+        <p>Links to some great lifting blogs until we get going with this</p>
+    </div>
+</body>
+</html>
